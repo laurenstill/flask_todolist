76df129789da04d2de7cc935989534ea5ecf6a4f
Student <student@Administrators-Mac-mini-3.local>
2012-07-02 18:02:17 -0700

    start of list

--- todolist/webapp.py
+++ todolist/webapp.py
@@ -1,5 +1,6 @@
-from flask import redirect, url_for, render_template
+from flask import redirect, url_for, render_template, request
 from model import app, Task
+import model
 
 @app.route("/")
 def home():
@@ -10,9 +11,26 @@ def home():
 
 @app.route("/add", methods=["GET"])
 def make_task():
-    return "Create a page for users to input a new task"
+	return render_template("make_tasks.html")
 
 @app.route("/add", methods=["POST"])
 def save_task():
-    return "Save a task"
+	new_task = Task(request.form['title'])
+	model.add(new_task)
+	model.save_all()
+	return "Saved this task"
+
+
+@app.route("/edit/<int:task_id>")
+def edit_task(task_id):
+	task = Task.query.get(task_id)
+	return "You entered %d in your url, %s"%(task_id, task)
+
+# @app.route("/completed", methods=["POST"])
+# def completed_task():
+# 	completed_task = Task.complete(request.for)
+
+# 	return "Marked this task completed."
+
+
 
