953a22043e7064c6f32063e058fae788c0f19a17
Student <student@Administrators-Mac-mini-3.local>
2012-07-06 17:45:28 -0700

    friday

--- todolist/webapp.py
+++ todolist/webapp.py
@@ -4,8 +4,8 @@ import model
 
 @app.route("/")
 def home():
-    return_str = ""
-    tasks = Task.query.all()
+    return_str = "" #this is a string 
+    tasks = Task.query.all() #list
     return render_template("list_tasks.html", tasks=tasks)
 
 
@@ -15,15 +15,20 @@ def make_task():
 
 @app.route("/add", methods=["POST"])
 def save_task():
-	new_task = Task(request.form['title'])
+	new_task = Task(request.form['title']) #string from dict
+	notes = request.form['notes'] #string from dict
+	new_task.notes = notes #object new_task with attribute notes
 	model.add(new_task)
 	model.save_all()
+
+	# return "Saved, theoretically"
 	return redirect(url_for("home"))
 
 
 @app.route("/edit/<int:task_id>", methods = ["GET"])
 def edit_task(task_id):
 	edit_task = Task.query.get(task_id)
+
 	return render_template("edit_tasks.html", task=edit_task)
 
 @app.route("/finished_tasks", methods = ["GET"])
@@ -32,9 +37,23 @@ def finished_task():
 
 @app.route("/finished_tasks/<int:task_id>", methods=["POST"])
 def save_finished_task(task_id):
+	"""Get an existing task, and change task title, notes, or 'doneness' level"""
 	task = Task.query.get(task_id)
+	
 	if request.form.get("done"):
 		task.complete()
+
+	task.title = request.form["title"]
+	task.notes = request.form["notes"]
+
+
+	print task.title
+	print task.notes
+	print task.done
+
+	
+	# if request.form.get("edit"):
+	# 	task.add()
 	# new_task = Task.complete(finished_task())
 	# model.add(new_task)
 	model.save_all()
